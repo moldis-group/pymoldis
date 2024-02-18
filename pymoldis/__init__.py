75c1203ac41c69a277dc48579e0629d8ef57da98
Atreyee Majumdar <154298872+atreyeem99@users.noreply.github.com>
2024-02-18 16:16:12 +0530

    am: makexyz function added

--- pymoldis/__init__.py
+++ pymoldis/__init__.py
@@ -1,2 +1,3 @@
 from .pymoldis_getdata import get_data
 from .pymoldis_utils import headers
+from .pymoldis_utils import makexyz
--- pymoldis/pymoldis_utils.py
+++ pymoldis/pymoldis_utils.py
@@ -23,3 +23,39 @@ def headers():
     '''
     return logo, header
 
+
+def makexyz(index,df,filename):
+
+    import numpy as np
+
+    mol=df.iloc[index,:]
+
+    ele=mol['atoms']
+    coord=mol['coords(Ang)']
+
+    ele=ele.strip(']').strip('[').strip(',').split(',')
+    atoms=[]
+    for iele in ele:
+        atoms.append(iele.strip("'"))
+
+    coord=coord.strip(']').strip('[').strip(',').split(',')
+    xyz=[]
+    for icoord in coord:
+        xyz.append(float(icoord.strip("'").strip(']').strip('[')))
+
+    xyz=np.array(xyz)
+    xyz=np.reshape(xyz,[4,3])
+
+    xyzfile=open('filename','w')
+
+    print(len(atoms))
+    xyzfile.write(str(len(atoms)))
+    print(filename)
+    xyzfile.write(filename)
+    for i,atom in enumerate(atoms):
+        print('{:s}{:15.8f}{:15.8f}{:15.8f}'.format(atom,xyz[i][0],xyz[i][1],xyz[i][2]))
+        xyzfile.write('{:s}{:15.8f}{:15.8f}{:15.8f}'.format(atom,xyz[i][0],xyz[i][1],xyz[i][2]))
+
+    xyzfile.close()
+
+    return
