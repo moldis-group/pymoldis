2b80140f8f317185a06674904e55ba29fca93c49
Raghu <62787956+raghurama123@users.noreply.github.com>
2024-02-21 22:45:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymoldis',
-    version='0.1',
+    version='1.0.1',
     packages=find_packages(),
     package_data={'pymoldis': ['data/*']},
     include_package_data=True,
