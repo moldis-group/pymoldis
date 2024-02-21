a6cea5bd74ba1e2a696a54ca00748c4b385195dd
raghurama123 <ramakrishnan@tifrh.res.in>
2024-02-22 03:23:32 +0530

    rr: changes to setup

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version='1.0.1',
     packages=find_packages(),
     package_data={'pymoldis': ['data/*']},
-    author = 'Raghunathan Ramakrishnan'
-    author_email = 'raghu.rama.chem@gmail.com'
+    author='Raghunathan Ramakrishnan'
+    author_email='raghu.rama.chem@gmail.com'
     include_package_data=True,
-    url = 'https://github.com/raghurama123/qmlspectrum'
-    license = 'MIT License'
-    description = 'A Python suite for data-mining the Quantum Chemistry Big Data developed through the MolDis project (https://moldis.tifrh.res.in/)'
-    long_desc_type = "text/markdown"
-    install_requires = [ 'pandas', 'numpy' ]
+    url='https://github.com/raghurama123/qmlspectrum'
+    license='MIT License'
+    description='A Python suite for data-mining the Quantum Chemistry Big Data developed through the MolDis project (https://moldis.tifrh.res.in/)'
+    long_desc_type="text/markdown"
+    install_requires=[ 'pandas', 'numpy' ]
     include_package_data=True,
 )
 
