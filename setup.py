db8442d5c1bd37bf5971a73b9ed665bf79ef67a5
Raghu <ramakrishnan@tifrh.res.in>
2024-04-27 12:07:07 +0530

    rr: small edits in setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     author='Raghunathan Ramakrishnan',
     author_email='raghu.rama.chem@gmail.com',
     include_package_data=True,
-    url='https://github.com/raghurama123/qmlspectrum'
-    license='MIT License'
-    description='A Python suite for data-mining the Quantum Chemistry Big Data developed through the MolDis project (https://moldis.tifrh.res.in/)'
-    long_desc_type="text/markdown"
-    install_requires=[ 'pandas', 'numpy' ]
+    url='https://github.com/raghurama123/qmlspectrum',
+    license='MIT License',
+    description='A Python suite for data-mining the Quantum Chemistry Big Data developed through the MolDis project (https://moldis.tifrh.res.in/)',
+    long_desc_type="text/markdown",
+    install_requires=[ 'pandas', 'numpy' ],
     include_package_data=True,
 )
 
