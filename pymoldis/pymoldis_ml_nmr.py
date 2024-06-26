25be92c9403dd62743e7a8fc496fbe53e56009c0
raghuramakrishnan <ramakrishnan@tifrh.res.in>
2024-06-26 18:51:21 +0530

    rr: models added

--- pymoldis/pymoldis_ml_nmr.py
+++ pymoldis/pymoldis_ml_nmr.py
@@ -38,7 +38,7 @@ def direct_ml(ele,rep,target,xyzfile):
             model=os.path.join(data_folder, 'bigqm7w_nmr_ml_models/H_model_direct_MP2.csv')
 
     elif ele == 'C' and rep == 'bob3' :
-        sigma = 519.45058153
+        sigma = 595.26753085
         lpnorm = 1
         descfile=os.path.join(data_folder, 'bigqm7w_nmr_ml_models/C_uff_Bob3.dat')
         if target == 'CCSD(T)':
@@ -81,58 +81,73 @@ def direct_ml(ele,rep,target,xyzfile):
         elif target == 'MP2':
             model=os.path.join(data_folder, 'bigqm7w_nmr_ml_models/F_model_direct_MP2.csv')
 
+    #=== calculate descriptor for query
     desc = pym.descriptor_gen(ele, xyzfile, rep)
 
-    at_desc=desc[0]
+    pred_q=[]
 
-    NNZ=0
-    for i,idesc in enumerate(at_desc):
-        if abs(idesc) > ZERO:
-            NNZ=NNZ+1
-
-    ind_CM=[]
-    for i,idesc in enumerate(at_desc):
-        if abs(idesc) > ZERO:
-            ind_CM.append(i)
-   
-    CM=[]
-    for idesc in at_desc:
-        if abs(idesc) > ZERO:
-            CM.append(idesc)
-
-
+    #=== read the model
     df = pd.read_csv(model,header=None)
-
+    
     coeff=np.array(df[0])
 
-
+    #=== read descriptors for the training set
     with open(descfile, 'r') as file:
         lines = file.readlines()
 
-    pred=0
+    for i_query in range(len(desc)):
 
-    for i,line in enumerate(lines):
-   
-        line=line.split()
-      
-        NNZ_t=int(line[0])
-      
-        ind_CM_t=[]
-        for t in range(NNZ_t):
-            ind_CM_t.append(int(line[t+1]))
-      
-        CM_t=[]
-        for t in range(NNZ_t):
-            CM_t.append(float(line[t+NNZ_t+1]))
-      
-        dij=dij_sparse(NNZ, NNZ_t, ind_CM, ind_CM_t, CM, CM_t, lpnorm)
-      
-        pred=pred+coeff[i]*np.exp(-dij/sigma)
+        at_desc=desc[i_query]
+    
+        #=== sparsify, get NNZ, ind_CM, CM
+        NNZ=0
+        for i,idesc in enumerate(at_desc):
+            if abs(idesc) > ZERO:
+                NNZ=NNZ+1
+    
+        ind_CM=[]
+        for i,idesc in enumerate(at_desc):
+            if abs(idesc) > ZERO:
+                ind_CM.append(i)
+       
+        CM=[]
+        for idesc in at_desc:
+            if abs(idesc) > ZERO:
+                CM.append(idesc)
+    
+        #=== prediction
+        pred=0
+    
+        #=== sum over training
+        for i,line in enumerate(lines):
+       
+            #=== load NNZ, ind_CM, CM and each training entry
+            line=line.split()
+          
+            NNZ_t=int(line[0])
+          
+            ind_CM_t=[]
+            for t in range(NNZ_t):
+                ind_CM_t.append(int(line[t+1]))
+          
+            CM_t=[]
+            for t in range(NNZ_t):
+                CM_t.append(float(line[t+NNZ_t+1]))
+    
+            #**** remove later ****
+            #CM_t[NNZ_t-1]=CM_t[NNZ_t-1]+1
+          
+            dij=dij_sparse(NNZ, NNZ_t, ind_CM, ind_CM_t, CM, CM_t, lpnorm)
+          
+            pred=pred+coeff[i]*np.exp(-dij/sigma)
 
-
+        pred_q.append(pred)
+    
+    
     with open(xyzfile, 'r') as file:
         lines = file.readlines()
-   
+    
+    ipred=0
     for iline, line in enumerate(lines):
         line=line.strip()
         if iline <= 1:
@@ -140,7 +155,8 @@ def direct_ml(ele,rep,target,xyzfile):
         else:
             qele=line[0]
             if qele == ele:
-                print(line,pred)
+                print(line,pred_q[ipred])
+                ipred=ipred+1
             else:
                 print(line)
 
